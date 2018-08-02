72041e68f7485150f0556c5f106a17ddfd570416
Bijon Miles <bijonmiles01@gmail.com>
2018-08-02 18:11:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ var specialGuest = "Neil deGrasse Tyson"
 var greetSpecialGuest = "" + specialGuest + "!";
 
 var topic = "space";
-
+https://learn.co/tracks/technical-application-javascript/introduction/welcome/strings-lab?batch_id=441&track_id=18313#
 var conversation = `${topic}`;
