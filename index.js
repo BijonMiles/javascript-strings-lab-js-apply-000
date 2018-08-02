f959ed47322593930f4cd4bc9e7f346d05c1f7d8
Bijon Miles <bijonmiles01@gmail.com>
2018-08-02 18:27:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var SpecialGuest =  "" + specialGuest + "!";
+var greetingSpecialGuest =  "" + specialGuest + "!";
 
 var topic = "space";
 
